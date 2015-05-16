742b41a09c64f4852f7df66f7fd2d5c306a12dc9
Gabriel Pickl <peacemotion@gmail.com>
2015-05-16 03:25:59 +0200

    Added square.

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,9 @@
-main = undefined
+import FRP.Helm
+import qualified FRP.Helm.Mouse as Mouse(position)
+import qualified FRP.Helm.Window as Window(dimensions)
+
+main = do
+  run defaultConfig $ render <$> Window.dimensions <*> Mouse.position
+
+render (w, h) (x, y) = collage w h [obj]
+  where obj = move (fromIntegral x, fromIntegral y) (rect 10.0 10.0 |> filled red)
