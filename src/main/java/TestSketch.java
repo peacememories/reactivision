5177f39dd6fee609560ae87f7a20a41a0d6878f4
moru <kevin.singer@student.tuwien.ac.at>
2015-05-23 15:04:02 +0200

    Added Box2D to build. Some refactoring.

--- src/main/java/TestSketch.java
+++ src/main/java/TestSketch.java
@@ -2,13 +2,12 @@ import processing.core.*;
 import shiffman.box2d.*;
 
 public class TestSketch extends PApplet {
- 
+
   // code taken from:
   // https://processing.org/tutorials/eclipse/
   // (there's more there)
-
   public void setup() {
-    //Box2DProcessing b = new Box2DProcessing(this);
+    Box2DProcessing b = new Box2DProcessing(this);
     size(200,200);
     background(0);
   }
@@ -16,7 +15,8 @@ public class TestSketch extends PApplet {
   public void draw() {
     stroke(255);
     if (mousePressed) {
-      line(mouseX,mouseY,pmouseX,pmouseY);
+      line(mouseX, mouseY,
+           pmouseX,pmouseY);
     }
   }
 
