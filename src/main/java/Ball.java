8b24025f45e2e20e586240d24ab308442c06fe6d
moru <kevin.singer@student.tuwien.ac.at>
2015-05-23 20:15:37 +0200

    Extracted ball-code into it's own class.

---
+++ src/main/java/Ball.java
@@ -0,0 +1,70 @@
+/**
+ * Created by moru on 5/23/2015.
+ */
+
+import processing.core.*;
+import shiffman.box2d.*;
+
+import org.jbox2d.collision.shapes.*;
+import org.jbox2d.common.*;
+import org.jbox2d.dynamics.*;
+
+public class Ball {
+
+    Body body;
+    int x = 100;
+    int y = 50;
+
+    Box2DProcessing box2d;
+
+    public Ball(Box2DProcessing box2dProcessing, PApplet p) {
+
+        box2d = box2dProcessing;
+
+        BodyDef bd = new BodyDef();
+        bd.position.set(box2d.coordPixelsToWorld(x,y));
+        bd.type = BodyType.DYNAMIC;
+
+        CircleShape circle = new CircleShape();
+        circle.m_p.set(3.0f, 3.0f);
+        circle.m_radius = 0.5f;
+
+        FixtureDef fd = new FixtureDef();
+        fd.shape = circle;
+        fd.density = 0;
+        fd.friction = 0.3f;
+        fd.restitution = 0.5f;
+
+        body = box2d.createBody(bd);
+        body.createFixture(fd);
+
+        // Give it some initial random velocity
+        body.setLinearVelocity(new Vec2(p.random(-5, 5), p.random(2, 5)));
+        body.setAngularVelocity(p.random(-5, 5));
+
+        System.out.println(body);
+    }
+
+    /**
+     * Renders the Ball onto <code>p</code>'s canvas.
+     * @param p the PApplet to serve as canvas
+     */
+    public void render(PApplet p){
+        // ------------- DRAW DEMO CIRCLE -------------------
+
+        // We look at each body and get its screen position
+        Vec2 pos = box2d.getBodyPixelCoord(body);
+        System.out.println(pos);
+        // Get its angle of rotation
+        float a = body.getAngle();
+
+        p.rectMode(p.CENTER);
+        p.pushMatrix();
+        p.translate(pos.x, pos.y);
+        p.rotate(-a);
+        p.fill(210);
+        p.stroke(0);
+        p.ellipse(0, 0, 10, 10);
+        p.popMatrix();
+    }
+}
--- src/main/java/TestSketch.java
+++ src/main/java/TestSketch.java
@@ -11,16 +11,10 @@ public class TestSketch extends PApplet {
   // A reference to our box2d world
   Box2DProcessing box2d;
 
+  Ball ball;
+
   public TestSketch() {
     super();
-
-
-    // ---------- Box2D-Processing Setup ----
-
-    box2d = new Box2DProcessing(this);
-    box2d.createWorld();
-    // We are setting a custom gravity
-    box2d.setGravity(0, -20);
   }
 
 	// A list we'll use to track fixed objects
@@ -36,59 +30,26 @@ public class TestSketch extends PApplet {
     background(0);
     smooth();
 
-    // ---------- INIT DEMO CIRCLE ---------
+    // ---------- Box2D-Processing Setup ----
+    box2d = new Box2DProcessing(this);
+    box2d.createWorld();
+    // We are setting a custom gravity
+    box2d.setGravity(0, -20);
 
-    BodyDef bd = new BodyDef();
-    bd.position.set(box2d.coordPixelsToWorld(x,y));
-    bd.type = BodyType.DYNAMIC;
-
-    CircleShape circle = new CircleShape();
-    circle.m_p.set(3.0f, 3.0f);
-    circle.m_radius = 0.5f;
-
-    FixtureDef fd = new FixtureDef();
-    fd.shape = circle;
-    fd.density = 0;
-    fd.friction = 0.3f;
-    fd.restitution = 0.5f;
-
-    body = box2d.createBody(bd);
-    body.createFixture(fd);
-
-    // Give it some initial random velocity
-    body.setLinearVelocity(new Vec2(random(-5, 5), random(2, 5)));
-    body.setAngularVelocity(random(-5, 5));
+    // ---------- Creating Game Pieces ------
+    ball = new Ball(box2d, this);
   }
 
   // ------- DEMO CIRCLE VARS ---------------
-  Body body;
-  int x = 100;
-  int y = 50;
-
   public void draw() {
-    background(0);
+    background(30);
 
     // We must always step through time!
     box2d.step(1.0f/60,10,10);
 
     // TODO if there should be any shapes that can leave the screen: delete them now
 
-    // ------------- DRAW DEMO CIRCLE -------------------
-
-    // We look at each body and get its screen position
-    Vec2 pos = box2d.getBodyPixelCoord(body);
-    System.out.println(pos);
-    // Get its angle of rotation
-    float a = body.getAngle();
-
-    rectMode(CENTER);
-    pushMatrix();
-    translate(pos.x, pos.y);
-    rotate(-a);
-    fill(175);
-    stroke(0);
-    ellipse(0, 0, 10, 10);
-    popMatrix();
+    ball.render(this);
   }
 
   public static void main(String args[]) {
