22bdeec4aa1e9c5959bfc01466cf69fd0d6a5e4a
moru <kevin.singer@student.tuwien.ac.at>
2015-05-27 00:46:43 +0200

    No waiting after random move.

--- src/main/java/RandomAgent.java
+++ src/main/java/RandomAgent.java
@@ -6,16 +6,21 @@ import java.util.Random;
 
 public class RandomAgent {
     private Random random;
-    private Agent agent;
     private float secondsToGoalChange = 0.0f;
-    private int screenWidth, screenHeight;
+    private final int screenWidth, screenHeight;
+
+    // controls whether the agent is in random mode (just wandering) or moving towards a goal
+    private boolean idle = true;
+
+    private float speed = 0.0f;
+    private PVector position, goal;
 
     public RandomAgent(long seed, int screenWidth, int screenHeight) {
         random = new Random(seed);
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
 
-        PVector pos = randomVec2(screenWidth, screenHeight);
+        PVector pos = randomGoal();
 
         // crosses table in 5 to 9 seconds if moving perfectly along axis
         float v = Math.min(screenHeight, screenWidth) /
@@ -23,28 +28,66 @@ public class RandomAgent {
 
         //System.out.println("With speed: " + v);
 
-        agent = new Agent(pos).setSpeed(v);
+        this.position = pos;
+        setSpeed(v);
     }
 
     public void update(float deltaSeconds) {
         secondsToGoalChange -= deltaSeconds;
-        if(secondsToGoalChange < 0) {
+        if(secondsToGoalChange < 0 && idle) {
             // run 3 to 11 seconds in the same direction for the next goal
             secondsToGoalChange = 3 + random.nextFloat() * 8;
-            agent.setGoal(randomVec2(screenWidth, screenHeight));
+            setGoal(randomGoal());
         }
-        agent.update(deltaSeconds);
 
+        // move towards goal -----
+
+        PVector path = goal.get();
+        path.sub(position);
+
+        PVector direction = path.get();
+        direction.normalize();
+
+        PVector move = direction.get();
+        move.mult(deltaSeconds * speed);
+
+        if(path.mag() < move.mag()) {
+            // we've reached the goal
+            this.position = goal.get();
+            idle = true; //change into idle mode
+            setGoal(randomGoal());
+        } else {
+            // move closer
+            this.position.add(move);
+        }
     }
 
-    private PVector randomVec2(int xMax, int yMax) {
-        int x = random.nextInt(xMax);
-        int y = random.nextInt(yMax);
+    private PVector randomGoal() {
+        int x = random.nextInt(screenWidth);
+        int y = random.nextInt(screenHeight);
         return new PVector(x, y);
     }
 
-    public PVector getPosition(){
-        return agent.getPosition();
+    public RandomAgent setGoal(PVector goal) {
+        this.idle = false;
+        this.goal = goal;
+        return this;
     }
+
+    /**
+     * @param speed in units per second
+     * @return
+     */
+    public RandomAgent setSpeed(float speed) {
+        this.speed = speed;
+        return this;
+    }
+
+    public PVector getPosition(){
+        return position.get();
+    }
+
+
+
 }
 
