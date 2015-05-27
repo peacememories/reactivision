e0faa17efaccad5298832cb761f00b8d4ca5188b
Gabriel Pickl <peacemotion@gmail.com>
2015-05-27 02:27:10 +0200

    Intermediate changes.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+/**
+ * Created by gabriel on 27/05/2015.
+ */
+public class Main extends PApplet {
+    @Override
+    public void setup() {
+        size(800,600);
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
--- src/main/java/widgets/Graph.java
+++ src/main/java/widgets/Graph.java
@@ -7,6 +7,21 @@ import util.Renderable;
  * Created by gabriel on 26/05/2015.
  */
 public class Graph implements Renderable {
+    private float width, height;
+
+    public Graph(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setSize(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public float getWidth() { return width; }
+    public float getHeight() { return height; }
+
     public void render(PApplet context) {
 
     }
